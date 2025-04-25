2037ff684a187ccd0a2be5865b8961848bc0fe2b
Batuhan Ergüven <gslibatuhan13@gmail.com>
2025-04-25 20:17:41 +0300

    Theme
    The app's basic color palettes

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+    // Diğer light tema ayarları
+  );
+
+  static final ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.blue,
+    // Diğer dark tema ayarları
+  );
+}
